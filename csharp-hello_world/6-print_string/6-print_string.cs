3ebaf89b035c81307e856c7c0612ca407dcad008
Christian-Ashley <Christianashley86@gmail.com>
2024-01-04 20:12:39 -0600

    task 6

---
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,16 @@
+﻿﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		 // Print the value of str 3 times
+        Console.WriteLine(str + str + str);
+        Console.WriteLine();
+
+        // Print the first 9 characters of str
+        Console.WriteLine(str.Substring(0, 9));
+        Console.WriteLine();
+        }
+}
