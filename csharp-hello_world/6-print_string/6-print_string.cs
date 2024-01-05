f4e11f99f9dbf9b1218456db7cd7aab7503cb5a0
Christian-Ashley <Christianashley86@gmail.com>
2024-01-04 20:14:55 -0600

    try this

--- csharp-hello_world/6-print_string/6-print_string.cs
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -7,10 +7,8 @@ class Program
 		string str = "Holberton School";
 		 // Print the value of str 3 times
         Console.WriteLine(str + str + str);
-        Console.WriteLine();
 
         // Print the first 9 characters of str
         Console.WriteLine(str.Substring(0, 9));
-        Console.WriteLine();
         }
 }
