2d9bd6521e62d780b5ec66c8676e17f59ef2ffdf
Christian-Ashley <Christianashley86@gmail.com>
2024-01-05 17:15:22 -0600

    fixed math and zero string

--- csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -8,13 +8,17 @@ class Program
         int number = rndm.Next(-10000, 10000);
         int lastDigit = Math.Abs(number % 10);
         Console.Write("The last digit of " + number + " is ");
+        if (lastDigit < 0)
+        {
+            lastDigit *= -1;
+        }
         if (lastDigit > 5)
         {
             Console.WriteLine(lastDigit + " and is greater than 5");
         }
         else if (lastDigit == 0)
         {
-            Console.WriteLine(lastDigit + " and is zero");
+            Console.WriteLine(lastDigit + " and is 0");
         }
         else 
         {
