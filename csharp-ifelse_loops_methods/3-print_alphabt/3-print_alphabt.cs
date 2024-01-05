e1f3d56bf61dc1c2317be6c9021deec892cdb4e5
Christian-Ashley <Christianashley86@gmail.com>
2024-01-05 17:54:06 -0600

    task 3

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char c = 'a'; c <= 'z'; c++)
+{
+    if (c != 'e' && c != 'q')
+    {
+        Console.Write(c);
+    }
+}
+        }
+    }
+}
