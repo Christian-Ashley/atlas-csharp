47cf38d9cb66cdc3f5d9a0aba8d60ef65ef361e3
Christian-Ashley <Christianashley86@gmail.com>
2024-01-05 22:42:50 -0600

    try this

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -6,10 +6,16 @@ public class Array
 {
     public static int[] CreatePrint(int size)
     {
+        {
         if (size < 0)
         {
             Console.WriteLine("Size cannot be negative");
             return null;
+        }
+        else if (size == 0)
+        {
+            Console.WriteLine();
+            return new int[0];
         }
         else
         {
@@ -29,6 +35,7 @@ public class Array
             Console.WriteLine();
             return newArray;
         }
+    }
     }
 }
 }
