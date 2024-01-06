f144ca931c3002844e4878603c5efa7f25f9d8e6
Christian-Ashley <Christianashley86@gmail.com>
2024-01-05 22:39:18 -0600

    how about now

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -11,18 +11,20 @@ public class Array
             Console.WriteLine("Size cannot be negative");
             return null;
         }
-        else if (size == 0)
-        {
-            Console.WriteLine();
-            return new int[0];
-        }
         else
         {
             int[] newArray = new int[size];
             for (int i = 0; i < size; i++)
             {
                 newArray[i] = i;
-                Console.Write(newArray[i] + " ");
+                if (i == size - 1)
+                {
+                    Console.Write(newArray[i]);
+                }
+                else
+                {
+                    Console.Write(newArray[i] + " ");
+                }
             }
             Console.WriteLine();
             return newArray;
