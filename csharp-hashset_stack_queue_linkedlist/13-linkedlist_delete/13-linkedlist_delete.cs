80e2161433c260ce5bd8897b4b02580fb9907a60
Christian-Ashley <Christianashley86@gmail.com>
2024-01-09 16:03:23 -0600

    task 13

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> current_node = myLList.First;
+        for (int i = 0; i <= index; i++)
+        {
+            if (current_node == null)
+            {
+                return;
+            }
+            if (i == index)
+            {
+                myLList.Remove(current_node);
+            }
+            current_node = current_node.Next;
+        }
+    }
+}
