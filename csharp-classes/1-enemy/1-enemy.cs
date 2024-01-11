bf1f4cb53c699e13a7d5227eb3ac5bbf1f6c9a35
Christian-Ashley <Christianashley86@gmail.com>
2024-01-10 20:49:55 -0600

    task 1

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Defines a Zombie</summary>
+    public class Zombie
+    {
+        /// <summary>Represents Zombies Health</summary>
+        public int health;
+
+        /// <summary>Zombie Constructor</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
