582d7048db4f4e5a9842438d6c1802b05ec0ff86
Christian-Ashley <Christianashley86@gmail.com>
2024-01-10 19:12:30 -0600

    task 1

---
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Int
+{
+    public static void divide(int a, int b)
+    {
+        try
+        {
+            int result = a / b;
+            Console.WriteLine($"{a} / {b} = {result}");
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine();
+        }
+    }
+}
