8d0f460153bc28a78064c0edc716334523fc7858
Christian-Ashley <Christianashley86@gmail.com>
2024-01-16 01:29:54 -0600

    task 2

---
+++ csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,53 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace MyMath.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Max_MaxMiddle_ReturnsMaxInt()
+        {
+            List<int> nums = new List<int> { 2, 4, 8, 3, 1};
+            int max = Operations.Max(nums);
+            Assert.AreEqual(8, max);        
+        }
+
+        [Test]
+        public void Max_MaxEnd_ReturnsMaxInt()
+        {
+            List<int> nums = new List<int> { 2, 4, 1, 3, 8};
+            int max = Operations.Max(nums);
+            Assert.AreEqual(8, max);        
+        }
+
+         [Test]
+        public void Max_MaxStart_ReturnsMaxInt()
+        {
+            List<int> nums = new List<int> { 8, 4, 2, 3, 1};
+            int max = Operations.Max(nums);
+            Assert.AreEqual(8, max);        
+        }
+
+        [Test]
+        public void Max_ContainsNegitive_ReturnsMaxInt()
+        {
+            List<int> nums = new List<int> { 2, -9, 8, 3, 1};
+            int max = Operations.Max(nums);
+            Assert.AreEqual(8, max);        
+        }
+
+        [Test]
+        public void Max_EmptyList_ReturnsZero()
+        {
+            List<int> nums = new List<int>();
+            int max = Operations.Max(nums);
+            Assert.AreEqual(0, max);
+        }
+    }
+}
