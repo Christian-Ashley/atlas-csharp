a135ec877358be20695a51e4210d963fc70502ab
Christian-Ashley <Christianashley86@gmail.com>
2024-01-17 10:45:41 -0600

    ok this should do it

--- csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
+++ csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -10,9 +10,45 @@ namespace Text.Tests
         }
 
         [Test]
-        public void Test1()
+        public void IsPalindrome_SingleWordCaps_RetunsTrue()
         {
-            Assert.Pass();
+            string str = "Racecar";
+            Assert.IsTrue(Str.IsPalindrome(str));
+        }
+
+        [Test]
+        public void IsPalindrome_SingleWord_RetunsTrue()
+        {
+            string str = "level";
+            Assert.IsTrue(Str.IsPalindrome(str));
+        }
+
+        [Test]
+        public void IsPalindrome_HasPunctuation_ReturnsTrue()
+        {
+            string str = "A man, a plan, a canal: Panama.";
+            Assert.IsTrue(Str.IsPalindrome(str));
+        }
+
+        [Test]
+        public void IsPalindrome_SingleWord_RetunsFalse()
+        {
+            string str = "kitten";
+            Assert.IsFalse(Str.IsPalindrome(str));
+        }
+
+        [Test]
+        public void IsPalindrome_HasPunctuation_ReturnsFalse()
+        {
+            string str = "Welp, I got nothing; same as you";
+            Assert.IsFalse(Str.IsPalindrome(str));
+        }
+
+        [Test]
+        public void IsPalindrome_EmptyString_ReturnsTrue()
+        {
+            string str = "";
+            Assert.IsTrue(Str.IsPalindrome(str));
         }
     }
 }
--- csharp-tdd/3-palindrome/Text/Text.cs
+++ csharp-tdd/3-palindrome/Text/Text.cs
@@ -1,8 +1,19 @@
-﻿using System;
+﻿﻿using System;
+using System.Linq;
 
 namespace Text
 {
-    public class Class1
+    /// <summary>
+    /// operations for strings
+    /// </summary>
+    public class Str
     {
+        /// <summary> determins if string is a palindrome </summary>
+        public static bool IsPalindrome(string s)
+        {
+            string convertedString = new string(s.ToLower().ToCharArray().Where(c => Char.IsLetterOrDigit(c)).ToArray());
+            string reversedString = new string(convertedString.Reverse().ToArray());
+            return convertedString == reversedString;
+        }
     }
 }
