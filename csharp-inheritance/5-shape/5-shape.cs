5271c73db1f25875a4724791b7666195bdb32352
Christian-Ashley <Christianashley86@gmail.com>
2024-01-17 19:47:02 -0600

    task 5

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary> represents a phalus </summary>
+class Shape
+{
+    /// <summary> area method got shafted </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
