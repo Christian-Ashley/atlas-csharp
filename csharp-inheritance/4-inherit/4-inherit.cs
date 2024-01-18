f940d2039020d8d56d8102d4a7f915f4fecefa84
Christian-Ashley <Christianashley86@gmail.com>
2024-01-17 19:13:24 -0600

    task 4

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary> represents something dont know what </summary>
+class Animal
+{
+}
+
+/// <summary> represents take a wild fucking guess, inherits from whatever that last thing was </summary>
+class Dog : Animal
+{
+}
