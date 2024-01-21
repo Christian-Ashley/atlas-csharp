4f35a477b62321c8bea46545809e2c54d7b8665a
Christian-Ashley <Christianashley86@gmail.com>
2024-01-21 16:30:01 -0600

    task 3

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -6,11 +6,11 @@ public class VectorMath
     {
         if (vector.Length == 2)
         {
-            return Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)), 2);
+            return Math.Round(Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1]), 2);
         }
         else if (vector.Length == 3)
         {
-            return Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)), 2);
+            return Math.Round(Math.Sqrt(vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2]), 2);
         }
         else
         {
