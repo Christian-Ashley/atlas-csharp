d1ee526725bf77d5950acddf9aaf6ad2726f0117
Christian-Ashley <Christianashley86@gmail.com>
2024-01-22 16:42:48 -0600

    task 16

---
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,26 @@
+﻿public class MatrixMath
+{
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        int rows = matrix.GetLength(0);
+        int columns = matrix.GetLength(1);
+
+        double[,] result = new double[rows, columns];
+
+        if (rows != 2 && rows != 3 && columns != 2 && columns != 3)
+        {
+            result = new double[,] { { -1 } };
+        }
+        else
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    result[i, j] = matrix[i, j] * scalar;
+                }
+            }
+        }
+        return result;
+    }
+}
