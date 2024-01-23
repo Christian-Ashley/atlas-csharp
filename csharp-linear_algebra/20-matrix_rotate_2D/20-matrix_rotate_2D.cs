a87a5a918a1f15038b0f684cd20873218bfbaa48
Christian-Ashley <Christianashley86@gmail.com>
2024-01-23 16:29:08 -0600

    try that

--- csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -1,26 +1,16 @@
 ﻿public class MatrixMath
 {
-    public static double[,] Rotate2D(double[,] matrix, double angle)
-    {
-        if (matrix.GetLength(0) != matrix.GetLength(1))
-        {
-            return new double[,] { { -1 } };
-        }
+   public static double[,] Rotate2D(double[,] matrix, double angle)
+	{
+		if (matrix.GetLength(1) > 2)
+			return new double[,] { { -1 } };
+		double[,] rotation = {{Math.Cos(angle), Math.Sin(angle)}, {-1 * Math.Sin(angle), Math.Cos(angle)}};
+		double[,] res = new double[2, 2];
 
-        double cosTheta = Math.Cos(angle);
-        double sinTheta = Math.Sin(angle);
-        int size = matrix.GetLength(0);
-        double[,] result = new double[size, size];
-
-        for (int i = 0; i < size; i++)
-        {
-            for (int j = 0; j < size; j++)
-            {
-                double x = i * cosTheta - j * sinTheta;
-                double y = i * sinTheta + j * cosTheta;
-                result[i, j] = matrix[(int)Math.Round(x), (int)Math.Round(y)];
-            }
-        }
-        return result;
-    }
+		for (int i = 0; i < 2; i++)
+			for (int j = 0; j < 2; j++)
+				for (int k = 0; k < 2; k++)
+					res[i, j] = Math.Round(res[i, j] + (matrix[i, k] * rotation[k, j]), 2);
+		return res;
+	} 
 }
